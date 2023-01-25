8fed87fe272c4b49008cd7cc5950a96cd0751966
Sale Djenic <aleksandardjenic@status.im>
2023-01-25 11:27:30 +0100

    feat: convert to a regular account method added

--- status_go/impl.nim
+++ status_go/impl.nim
@@ -81,7 +81,9 @@ proc validateNodeConfig*(configJSON: cstring): cstring {.importc: "ValidateNodeC
 
 proc loginWithKeycard*(accountData: cstring, password: cstring, keyHex: cstring): cstring {.importc: "LoginWithKeycard".}
 
-func convertToKeycardAccount*(keyStoreDir: cstring, accountData: cstring, settingsJSON: cstring, password: cstring, newPassword: cstring): cstring {.importc: "ConvertToKeycardAccount".}
+func convertToKeycardAccount*(accountData: cstring, settingsJSON: cstring, password: cstring, newPassword: cstring): cstring {.importc: "ConvertToKeycardAccount".}
+
+func convertToRegularAccount*(mnemonic: cstring, currPassword: cstring, newPassword: cstring): cstring {.importc: "ConvertToRegularAccount".}
 
 proc recover*(rpcParams: cstring): cstring {.importc: "Recover".}
 
